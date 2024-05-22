7a97cbfc56d1e87ccd8e4a2a803bc85528efd61a
Eryk Szpotanski <eszpotanski@antmicro.com>
2024-05-22 19:58:54 +0200

    docker-compose: run: Do not create volumes for hardlink worker
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -2,7 +2,6 @@
 set -eu
 
 worker_fuse="worker-fuse-ubuntu22-04"
-worker_hardlinking="worker-hardlinking-ubuntu22-04"
 fuse_dir_to_unmount="volumes/${worker_fuse}/build"
 sudo -v
 
@@ -11,12 +10,11 @@ setup () {
     set -x
 
     { sudo fusermount -u "$fuse_dir_to_unmount" && sleep 1; } || true
-    sudo rm -rf volumes/bb "volumes/${worker_fuse}" "volumes/${worker_hardlinking}"
+    sudo rm -rf volumes/bb "volumes/${worker_fuse}"
 
     mkdir -p volumes
     mkdir -m 0777 "volumes/${worker_fuse}" "volumes/${worker_fuse}"/{build,cas,cas/persistent_state}
-    mkdir -m 0777 "volumes/${worker_hardlinking}" "volumes/${worker_hardlinking}"/{build,cas,cas/persistent_state}
-    mkdir -m 0700 "volumes/${worker_fuse}/cache" "volumes/${worker_hardlinking}/cache"
+    mkdir -m 0700 "volumes/${worker_fuse}/cache"
     mkdir -p volumes/storage-{ac,cas}-{0,1}/persistent_state
     chmod 0700 volumes/storage-{ac,cas}-{0,1}/{,persistent_state}
 }
